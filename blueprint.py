57ade93817db86425279cd4e744903f4a984866b
begunew <bilguunsoninbayar@gmail.com>
2026-02-23 08:45:35 +0800

    Added comments

--- blueprint.py
+++ blueprint.py
@@ -20,21 +20,26 @@ class ShoppingCart:
         self.items.remove(item)
         print(item.name, "removed from the cart.")
 
-
+# Creating new objects from Item class
 item1 = Item("cola", 3000)
 item2 = Item("sprite", 3000)
 item3 = Item("fanta", 3000)
 
+# initializing/creating the ShoppingCart
 cart1 = ShoppingCart()
 
+# Adding new items into the shopping cart
 cart1.add_item(item1)
-cart1.add_item(item1)
-cart1.add_item(item1)
+cart1.add_item(item2)
+cart1.add_item(item3)
 
+# Showing all items in the cart
 cart1.show_items()
 
+# Removing the item: cola from the cart
 cart1.remove_item(item1)
 
+# Re-showing all the items in the cart, to confirm the cola was removed.
 cart1.show_items()
 
 
